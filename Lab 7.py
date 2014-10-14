19ef4bbefc99256ead2a71f86d6aa4a4a8f26840
lukecooper96 <luke.cooper96@gmail.com>
2014-10-14 13:46:03 -0500

    Lab 7

---
+++ Lab 7.py
@@ -0,0 +1,28 @@
+#x = 1
+
+#while (x < 300):
+    #print x
+    #x = x + 2
+
+#myList = [5,9,6,4,3,2,"hello","goodbye",7,"hasta luego"]
+#index = 0
+#while (index < len(myList)):
+    #print myList[index]
+    #index = index +1
+    
+#import random
+#rand = random.randint(0,50)
+#gamer = -1
+#print "Guess a number between zero and fifty."
+#while(gamer !=rand):
+    #gamer = int(raw_input())
+    #if gamer<rand:
+        #print "too low"
+    #if gamer>rand:
+        #print "too high"
+#print "___________________"
+#print "You guessed correctly!!!!!!!!!!!!!!!" 
+
+
+   
+
